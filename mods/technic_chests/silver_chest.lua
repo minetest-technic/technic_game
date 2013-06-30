217e7cf4a8be12a7d3d64790a366590375a249c4
RealBadAngel <mk@realbadangel.pl>
2013-06-30 16:26:09 +0200

    New way of changing chest's infotext

--- mods/technic_chests/silver_chest.lua
+++ mods/technic_chests/silver_chest.lua
@@ -1,5 +1,5 @@
 minetest.register_craft({
-	output = 'technic:silver_chest 1',
+	output = 'technic:silver_chest',
 	recipe = {
 		{'moreores:silver_ingot','moreores:silver_ingot','moreores:silver_ingot'},
 		{'moreores:silver_ingot','technic:copper_chest','moreores:silver_ingot'},
@@ -8,7 +8,7 @@ minetest.register_craft({
 })
 
 minetest.register_craft({
-	output = 'technic:silver_locked_chest 1',
+	output = 'technic:silver_locked_chest',
 	recipe = {
 		{'moreores:silver_ingot','moreores:silver_ingot','moreores:silver_ingot'},
 		{'moreores:silver_ingot','technic:copper_locked_chest','moreores:silver_ingot'},
@@ -17,7 +17,7 @@ minetest.register_craft({
 })
 
 minetest.register_craft({
-	output = 'technic:silver_locked_chest 1',
+	output = 'technic:silver_locked_chest',
 	recipe = {
 		{'default:steel_ingot'},
 		{'technic:silver_chest'},
@@ -33,6 +33,14 @@ minetest.register_craftitem(":technic:silver_locked_chest", {
 	stack_max = 99,
 })
 
+silver_chest_formspec = 
+				"invsize[11,10;]"..
+				"list[current_name;main;0,1;11,4;]"..
+				"list[current_player;main;0,6;8,4;]"..
+				"background[-0.19,-0.25,;11.4,10.75;ui_form_bg.png]"..
+				"background[0,1;11,4;ui_silver_chest_inventory.png]"..
+				"background[0,6;8,4;ui_main_inventory.png]"
+				
 minetest.register_node(":technic:silver_chest", {
 	description = "Silver Chest",
 	tiles = {"technic_silver_chest_top.png", "technic_silver_chest_top.png", "technic_silver_chest_side.png",
@@ -45,38 +53,34 @@ minetest.register_node(":technic:silver_chest", {
 	on_construct = function(pos)
 		local meta = minetest.env:get_meta(pos)
 		meta:set_string("formspec",
-				"invsize[11,10;]"..
+				silver_chest_formspec..
 				"label[0,0;Silver Chest]"..
-				"list[current_name;main;0,1;11,4;]"..
-				"list[current_player;main;0,6;8,4;]"..
-				"background[-0.19,-0.25,;11.4,10.75;ui_form_bg.png]"..
-				"background[0,1;11,4;ui_silver_chest_inventory.png]"..
-				"background[0,6;8,4;ui_main_inventory.png]")
+				"image_button[2.5,.1;.6,.6;pencil_icon.png;edit_infotext;]")
 		meta:set_string("infotext", "Silver Chest")
 		local inv = meta:get_inventory()
 		inv:set_size("main", 11*4)
 	end,
 	can_dig = chest_can_dig,
 
-	on_punch = function (pos, node, puncher)
-	        local meta = minetest.env:get_meta(pos);
-                meta:set_string("formspec", "hack:sign_text_input")
-	end,
-	
 	on_receive_fields = function(pos, formname, fields, sender)
-        local meta = minetest.env:get_meta(pos);
-      		fields.text = fields.text or ""
-		meta:set_string("text", fields.text)
-		meta:set_string("infotext", '"'..fields.text..'"')
-
-		meta:set_string("formspec",
-				"invsize[11,10;]"..
-				"label[0,0;Silver Chest]"..
-				"list[current_name;main;0,1;11,4;]"..
-				"list[current_player;main;0,6;8,4;]"..
-				"background[-0.19,-0.25,;11.4,10.75;ui_form_bg.png]"..
-				"background[0,1;11,4;ui_silver_chest_inventory.png]"..
-				"background[0,6;8,4;ui_main_inventory.png]")
+        local meta = minetest.env:get_meta(pos)
+      	local page="main"
+      	if fields.edit_infotext then 
+			page="edit_infotext"
+      	end
+      	if fields.save_infotext then 
+			meta:set_string("infotext",fields.infotext_box)
+      	end
+		local formspec = silver_chest_formspec.."label[0,0;Silver Chest]"
+		if page=="main" then
+			formspec = formspec.."image_button[2.5,.1;.6,.6;pencil_icon.png;edit_infotext;]"
+			formspec = formspec.."label[3,0;"..meta:get_string("infotext").."]"
+		end
+		if page=="edit_infotext" then
+			formspec = formspec.."image_button[2.5,.1;.6,.6;ok_icon.png;save_infotext;]"
+			formspec = formspec.."field[3.3,.2;6,1;infotext_box;Edit chest description:;"..meta:get_string("infotext").."]"
+		end			
+		meta:set_string("formspec",formspec)
 	end,
 
 	on_metadata_inventory_move = def_on_metadata_inventory_move,
@@ -102,41 +106,35 @@ minetest.register_node(":technic:silver_locked_chest", {
 	on_construct = function(pos)
 		local meta = minetest.env:get_meta(pos)
 		meta:set_string("formspec",
-				"invsize[11,10;]"..
+				silver_chest_formspec..
 				"label[0,0;Silver Locked Chest]"..
-				"list[current_name;main;0,1;11,4;]"..
-				"list[current_player;main;0,6;8,4;]"..
-				"background[-0.19,-0.25,;11.4,10.75;ui_form_bg.png]"..
-				"background[0,1;11,4;ui_silver_chest_inventory.png]"..
-				"background[0,6;8,4;ui_main_inventory.png]")
-		meta:set_string("infotext", "Silver Locked Chest")
+				"image_button[2.5,.1;.6,.6;pencil_icon.png;edit_infotext;]")
 		meta:set_string("owner", "")
 		local inv = meta:get_inventory()
 		inv:set_size("main", 11*4)
 	end,
 	can_dig = chest_can_dig,
 
-	on_punch = function (pos, node, puncher)
-	        local meta = minetest.env:get_meta(pos);
-                meta:set_string("formspec", "hack:sign_text_input")
-	end,
-	
 	on_receive_fields = function(pos, formname, fields, sender)
-        local meta = minetest.env:get_meta(pos);
-      		fields.text = fields.text or ""
-		meta:set_string("text", fields.text)
-		meta:set_string("infotext", '"'..fields.text..'"')
-
-		meta:set_string("formspec",
-				"invsize[11,10;]"..
-				"label[0,0;Silver Locked Chest]"..
-				"list[current_name;main;0,1;11,4;]"..
-				"list[current_player;main;0,6;8,4;]"..
-				"background[-0.19,-0.25,;11.4,10.75;ui_form_bg.png]"..
-				"background[0,1;11,4;ui_silver_chest_inventory.png]"..
-				"background[0,6;8,4;ui_main_inventory.png]")
+        local meta = minetest.env:get_meta(pos)
+      	local page="main"
+      	if fields.edit_infotext then 
+			page="edit_infotext"
+      	end
+      	if fields.save_infotext then 
+			meta:set_string("infotext",fields.infotext_box)
+      	end
+		local formspec = silver_chest_formspec.."label[0,0;Silver Locked Chest]"
+		if page=="main" then
+			formspec = formspec.."image_button[2.5,.1;.6,.6;pencil_icon.png;edit_infotext;]"
+			formspec = formspec.."label[3,0;"..meta:get_string("infotext").."]"
+		end
+		if page=="edit_infotext" then
+			formspec = formspec.."image_button[2.5,.1;.6,.6;ok_icon.png;save_infotext;]"
+			formspec = formspec.."field[3.3,.2;6,1;infotext_box;Edit chest description:;"..meta:get_string("infotext").."]"
+		end			
+		meta:set_string("formspec",formspec)
 	end,
-
 
 	allow_metadata_inventory_move = def_allow_metadata_inventory_move,
 	allow_metadata_inventory_put = def_allow_metadata_inventory_put,
