fbfa1e734fee5cc8ac5208beb262d6000431fc0e
ShadowNinja <noreply@gmail.com>
2013-07-11 13:51:26 -0400

    Use git submodules for mods
    Conflicts:
    	mods/digilines
    	mods/digilines/digilines/wire_std.lua
    	mods/digilines/digilines_lcd/init.lua
    	mods/digilines/digilines_lightsensor/init.lua
    	mods/digilines/digilines_rtc/init.lua

--- mods/mesecons/mesecons/settings.lua
+++
@@ -1,9 +0,0 @@
--- SETTINGS
-BLINKY_PLANT_INTERVAL = 3
-NEW_STYLE_WIRES  = true  -- true = new nodebox wires, false = old raillike wires
-PRESSURE_PLATE_INTERVAL = 0.1
-OBJECT_DETECTOR_RADIUS = 6
-PISTON_MAXIMUM_PUSH = 15
-MOVESTONE_MAXIMUM_PUSH = 100
-MESECONS_GLOBALSTEP = true	-- true = receptors/effectors won't be updated
-				-- until next globalstep, decreases server load
---
+++ mods/moonflower/README.md
@@ -0,0 +1,4 @@
+minetest_mods_moonflower
+========================
+
+Moonflower mod for Minetest
