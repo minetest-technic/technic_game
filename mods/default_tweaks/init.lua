ec09c5197d6128baeb3b5b93768242610392c6bc
ShadowNinja <shadowninja@minetest.net>
2014-01-25 14:52:13 -0500

    Pass all arguments on to old chest and furnace formspec functions

--- mods/default_tweaks/init.lua
+++ mods/default_tweaks/init.lua
@@ -54,8 +54,8 @@ default.chest_formspec = default.chest_formspec
 	.."background[0,6;8,4;ui_main_inventory.png]"
 
 local old_get_locked_chest_formspec = default.get_locked_chect_formspec
-function default.get_locked_chest_formspec(pos)
-	return old_get_locked_chest_formspec(pos)
+function default.get_locked_chest_formspec(...)
+	return old_get_locked_chest_formspec(...)
 		.."label[0,0;Wooden Locked Chest]"
 		.."background[-0.19,-0.25;8.4,10.75;ui_form_bg.png]"
 		.."background[0,1;8,4;ui_wooden_chest_inventory.png]"
@@ -73,8 +73,8 @@ default.furnace_inactive_formspec = default.furnace_inactive_formspec
 	.."background[0,1;8,4;ui_furnace_inventory.png]"
 
 local old_get_furnace_active_formspec = default.get_furnace_active_formspec
-function default.get_furnace_active_formspec(pos, percent)
-	return old_get_furnace_active_formspec(pos, percent)
+function default.get_furnace_active_formspec(...)
+	return old_get_furnace_active_formspec(...)
 		.."label[0,0;Furnace Active]"
 		.."background[-0.19,-0.25;8.4,10.75;ui_form_bg.png]"
 		.."background[0,6;8,4;ui_main_inventory.png]"
